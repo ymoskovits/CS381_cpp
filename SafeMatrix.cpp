016c5686e15e8a9d1ed4fe1fa5df71f6438f0dae
Julian Moskovits <julianmoskovits@Julians-MacBook-Pro.local>
2018-10-29 20:52:13 -0400

    first commit

---
+++ SafeMatrix.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+class SafeMatrix{
+public:
+	int xfirst, xlast, yfirst, ylast, xlen, ylen;
+	T **mp;
+
+
+	SafeMatrix(){
+		xfirst = 0;
+		xlast = -1;
+		yfirst = 0;
+		ylast = -1;
+		xlen = 0;
+		ylen = 0;
+		mp = nullptr;
+	}
+	void swap(SafeMatrix& a, SafeMatrix& b){
+		std::swap(a.xfirst, b.xfirst);
+		std::swap(a.xlast, b.xlast);
+		std::swap(a.yfirst, b.yfirst);
+		std::swap(a.ylast, b.ylast);
+		std::swap(a.xlen, b.xlen);
+		std::swap(a.ylen, b.ylen);
+
+		T** temp = a.mp;
+		a.mp = b.mp;
+		b.mp = temp;
+	}
+
+	//The bounds are inclusive.
+	SafeMatrix(int xfirst, int xlast, int yfirst, int ylast){
+		this->xfirst = xfirst;
+		this->xlast = xlast;
+		this->yfirst = yfirst;
+		this->ylast = ylast;
+
+		xlen = xlast - xfirst + 1;
+		ylen = ylast - yfirst + 1;
+
+		mp = new T*[xlen];
+		for(int i = 0; i < xlen; ++i){
+			mp[i] = new T[ylen];
+		}
+	}
+
+	SafeMatrix(SafeMatrix& other){
+		xfirst = other.xfirst;
+		xlast = other.xlast;
+		yfirst = other.yfirst;
+		ylast = other.ylast;
+		xlen = xlast - xfirst + 1;
+		ylen = ylast - yfirst + 1;
+
+		mp = new T*[xlen];
+		for(int i = 0; i < xlen; ++i){
+			mp[i] = new T[ylen];
+		}
+
+		for(int i = 0; i < xlen; ++i){
+			for(int j = 0; j < ylen; ++j){
+				mp[i][j] = other.mp[i][j];
+			}
+		}
+	}
+
+	SafeMatrix(SafeMatrix&& other){
+		xfirst = other.xfirst;
+		xlast = other.xlast;
+		yfirst = other.yfirst;
+		ylast = other.ylast;
+		xlen = xlast - xfirst + 1;
+		ylen = ylast - yfirst + 1;
+
+		mp = other.mp;
+		other.mp = nullptr;
+	}
+
+
+
+	~SafeMatrix(){
+		for(int i = 0; i < xlen; ++i){
+			delete [] mp[i];
+		}
+		delete [] mp;
+	}
+
+
+	SafeMatrix& operator=(const SafeMatrix other){
+
+		// swap(mp, other);
+		// return *this;
+
+
+		if(this == &other){
+			return *this;
+		}
+
+		for(int i = 0; i < xlen; ++i){
+			for(int j = 0; j < ylen; ++j){
+				delete [] mp[i];
+			}
+			delete [] mp;
+		}
+
+		xfirst = other.xfirst;
+		xlast = other.xlast;
+		yfirst = other.yfirst;
+		ylast = other.ylast;
+		xlen = xlast - xfirst + 1;
+		ylen = ylast - yfirst + 1;
+
+		mp = new T*[xlen];
+		for(int i = 0; i < xlen; ++i){
+			mp[i] = new T[ylen];
+		}
+
+		for(int i = 0; i < xlen; ++i){
+			for(int j = 0; j < ylen; ++j){
+				mp[i][j] = other.mp[i][j];
+			}
+		}
+
+		return *this;
+	}
+
+
+
+
+	class OpProxy{
+		T* specific_array;
+	public:
+
+		OpProxy(){
+			specific_array = nullptr;
+		}
+
+		OpProxy(T* arr){
+			specific_array = arr;
+		}
+
+		T& operator[](int index){
+			return specific_array[index];
+		}
+
+		~OpProxy(){
+
+		}
+	};
+
+	OpProxy operator[](int index){
+		OpProxy prox(mp[index - xfirst] - yfirst);
+		return prox;
+	}
+
+
+};
+
+
+int main(){
+	SafeMatrix<int> m(0, 2, 0, 2);
+
+	int count = 100;
+	for (int i = 0; i < 2; ++i){
+		for(int j = 0; j < 2; ++j){
+			m[i][j] = count += 700;
+		}
+	}
+
+	SafeMatrix<int> b(3, 9, 3, 9);
+	
+
+	for (int i = 3; i < 9; ++i){
+		for(int j = 3; j < 9; ++j){
+			b[i][j] = i * j;
+		}
+	}
+
+	for (int i = 0; i < 2; ++i){
+		for(int j = 0; j < 2; ++j){
+			cout << m[i][j] << endl;
+		}
+	}
+	cout << endl << endl;
+	for (int i = 3; i < 9; ++i){
+		for(int j = 3; j < 9; ++j){
+			cout << b[i][j] << endl;
+		}
+	}
+
+	swap(b, m);
+
+	// for (int i = 0; i < 2; ++i){
+	// 	for(int j = 0; j < 2; ++j){
+	// 		cout << m[i][j] << endl;
+	// 	}
+	// }
+	// cout << endl << endl;
+	// for (int i = 3; i < 9; ++i){
+	// 	for(int j = 3; j < 9; ++j){
+	// 		cout << b[i][j] << endl;
+	// 	}
+	// }
+
+	return 0;
+}
