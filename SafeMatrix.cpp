53a80b498ae120960290fcf41d3de0a67279d514
Julian Moskovits <julianmoskovits@Julians-MacBook-Pro.local>
2018-11-01 17:30:56 -0400

    messed up the x and y

--- SafeMatrix.cpp
+++ SafeMatrix.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <cstdlib> 
 using namespace std;
 
-template <typename T>
+template <class T>
 class SafeMatrix{
 public:
 	int xfirst, xlast, yfirst, ylast, xlen, ylen;
@@ -30,26 +31,35 @@ public:
 		if(xlen != other.ylen){
 			throw new exception;
 		}
-
+		int val;
 		SafeMatrix<T> res(0, ylen, 0, other.xlen);
-		// for(int i)
+		for(int i = 0; i <= res.ylen; ++i){
+			for(int j = 0; j <= res.xlen; ++j){
+				val = 0;
+				for(int k = 0; k <= xlen; ++k){
+					val += mp[i][k] * other.mp[k][j];
+				}
+				res[i][j] = val;
+
+			}
+		}
 
 		return res;
 
 	}
 
-	void swap(SafeMatrix& a, SafeMatrix& b){
-		std::swap(a.xfirst, b.xfirst);
-		std::swap(a.xlast, b.xlast);
-		std::swap(a.yfirst, b.yfirst);
-		std::swap(a.ylast, b.ylast);
-		std::swap(a.xlen, b.xlen);
-		std::swap(a.ylen, b.ylen);
+	// void swap(SafeMatrix& a, SafeMatrix& b){
+	// 	std::swap(a.xfirst, b.xfirst);
+	// 	std::swap(a.xlast, b.xlast);
+	// 	std::swap(a.yfirst, b.yfirst);
+	// 	std::swap(a.ylast, b.ylast);
+	// 	std::swap(a.xlen, b.xlen);
+	// 	std::swap(a.ylen, b.ylen);
 
-		T** temp = a.mp;
-		a.mp = b.mp;
-		b.mp = temp;
-	}
+	// 	T** temp = a.mp;
+	// 	a.mp = b.mp;
+	// 	b.mp = temp;
+	// }
 
 	//The bounds are inclusive.
 	SafeMatrix(int xfirst, int xlast, int yfirst, int ylast){
@@ -61,9 +71,9 @@ public:
 		xlen = xlast - xfirst + 1;
 		ylen = ylast - yfirst + 1;
 
-		mp = new T*[xlen];
-		for(int i = 0; i < xlen; ++i){
-			mp[i] = new T[ylen];
+		mp = new T*[ylen];
+		for(int i = 0; i < ylen; ++i){
+			mp[i] = new T[xlen];
 		}
 	}
 
@@ -75,13 +85,13 @@ public:
 		xlen = xlast - xfirst + 1;
 		ylen = ylast - yfirst + 1;
 
-		mp = new T*[xlen];
-		for(int i = 0; i < xlen; ++i){
-			mp[i] = new T[ylen];
+		mp = new T*[ylen];
+		for(int i = 0; i < ylen; ++i){
+			mp[i] = new T[xlen];
 		}
 
-		for(int i = 0; i < xlen; ++i){
-			for(int j = 0; j < ylen; ++j){
+		for(int i = 0; i < ylen; ++i){
+			for(int j = 0; j < xlen; ++j){
 				mp[i][j] = other.mp[i][j];
 			}
 		}
@@ -102,7 +112,7 @@ public:
 
 
 	~SafeMatrix(){
-		for(int i = 0; i < xlen; ++i){
+		for(int i = 0; i < ylen; ++i){
 			delete [] mp[i];
 		}
 		delete [] mp;
@@ -119,7 +129,7 @@ public:
 			return *this;
 		}
 
-		for(int i = 0; i < xlen; ++i){
+		for(int i = 0; i < ylen; ++i){
 			delete [] mp[i];
 		}
 		delete [] mp;
@@ -132,13 +142,13 @@ public:
 		xlen = xlast - xfirst + 1;
 		ylen = ylast - yfirst + 1;
 
-		mp = new T*[xlen];
-		for(int i = 0; i < xlen; ++i){
-			mp[i] = new T[ylen];
+		mp = new T*[ylen];
+		for(int i = 0; i < ylen; ++i){
+			mp[i] = new T[xlen];
 		}
 
-		for(int i = 0; i < xlen; ++i){
-			for(int j = 0; j < ylen; ++j){
+		for(int i = 0; i < ylen; ++i){
+			for(int j = 0; j < xlen; ++j){
 				mp[i][j] = other.mp[i][j];
 			}
 		}
@@ -177,26 +187,29 @@ public:
 		OpProxy prox(mp[index - xfirst] - yfirst);
 		return prox;
 	}
-	friend ostream& operator<<(ostream& os, const SafeMatrix& sm){
-		for(int i = sm.yfirst; i <= sm.ylast; ++i){
-			for(int j = sm.xfirst; j <= sm.xlast; ++j){
-				os << sm.mp[i][j] << " ";
-			}
-			os << endl;
-		}
-		return os;
-	}
+
+	// friend ostream& operator<< <T>(ostream& os, const SafeMatrix<T>& sm);
 
 };
 
-
-
+template<class T>
+ostream& operator<<(ostream& os, SafeMatrix<T>& sm){
+	cout << sm.yfirst << " " << sm.ylast << endl;
+	cout << sm.xfirst << " " << sm.xlast << endl;
+	for(int i = sm.yfirst; i <= sm.ylast; ++i){
+		for(int j = sm.xfirst; j <= sm.xlast; ++j){
+			cout << sm.mp[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return os;
+};
 
 int main(){
 	SafeMatrix<int> m(0, 2, 0, 2);
-	SafeMatrix<int> b(3, 9, 3, 9);
+	// SafeMatrix<int> b(3, 9, 3, 9);
 
-	int count = 100;
+	// int count = 100;
 
 	for (int i = m.yfirst; i <= m.ylast; ++i){
 		for(int j = m.xfirst; j <= m.xlast; ++j){
@@ -211,10 +224,17 @@ int main(){
 	}
 	cout << b << endl << m;
 
-	b = m;
+	// b = m;
 
-	cout << b << endl << m;
+	// cout << b ;//<< endl << m;
 
 
 	return 0;
 }
+
+
+
+
+
+
+
