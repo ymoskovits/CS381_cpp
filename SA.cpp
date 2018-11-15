7090bc996513f96d42d4fee0aa236d6397b507ec
Julian Moskovits <julianmoskovits@Julians-MacBook-Pro.local>
2018-11-15 16:56:29 -0500

    format SA

--- SA.cpp
+++ SA.cpp
@@ -11,28 +11,33 @@ public:
  // default constructor
  // allows for writing things like SA a;
 
- SA(){low=0; high=-1;p=NULL;}
+ SA(){
+ 	low=0;
+ 	high=-1;
+ 	p=NULL;
+ }
 
 
  // 2 parameter constructor lets us write
  // SA x(10,20);
 
  SA(int l, int h){
- if((h-l+1)<=0)
- {
- 	 cout<< "constructor error in bounds definition"<<endl;
-	 exit(1);}
-	 low=l;
-	 high=h;
-	 p=new int[h-l+1];
- }
-
+	if((h-l+1)<=0){
+	 	 cout<< "constructor error in bounds definition"<<endl;
+		 exit(1);
+	}
+	low=l;
+	high=h;
+	p=new int[h-l+1]; 
+}
 
  // single parameter constructor lets us
  // create a SA almost like a "standard" one by writing
  // SA x(10); and getting an array x indexed from 0 to 9
 
- SA(int i){low=0; high=i-1;
+ SA(int i){
+ 	low=0;
+ 	high=i-1;
  	p=new int[i];
  }
  // copy constructor for pass by value and
@@ -41,8 +46,9 @@ public:
  SA(const SA & s){
 	 int size=s.high-s.low+1;
 	 p=new int[size];
-	 for(int i=0; i<size; i++)
-	 p[i]=s.p[i];
+	 for(int i=0; i<size; i++){
+	 	p[i]=s.p[i];
+	 }
 	 low=s.low;
 	 high=s.high;
  }
@@ -55,10 +61,11 @@ public:
  //SA x(10,20); x[15]= 100;
 
  int& operator[](int i){
- if(i<low || i>high)
- {cout<< "index "<<i<<" out of range"<<endl;
- exit(1);}
- return p[i-low];
+	if(i<low || i>high){
+ 		cout<< "index "<<i<<" out of range"<<endl;
+ 		exit(1);
+ 	}
+	return p[i-low];
  }
  
 
@@ -66,38 +73,48 @@ public:
  // one SA to another
 
  SA & operator=(const SA & s){
- if(this==&s)return *this;
- delete [] p;
- int size=s.high-s.low+1;
- p=new int[size];
- for(int i=0; i<size; i++)
- p[i]=s.p[i];
- low=s.low;
- high=s.high;
- return *this;
+ 	if(this==&s){
+ 		return *this;
+ 	}
+	delete [] p;
+	int size=s.high-s.low+1;
+ 	p=new int[size];
+ 	for(int i=0; i<size; i++){
+ 		p[i]=s.p[i];
+ 	}
+	low=s.low;
+	high=s.high;
+	return *this;
  }
 
  // overloads << so we can directly print SAs
 
  friend ostream& operator<<(ostream& os, SA s);
 };
+
 ostream& operator<<(ostream& os, SA s){
-int size=s.high-s.low+1;
-for(int i=0; i<size; i++)
- cout<<s.p[i]<<endl;
-return os;
+	int size=s.high-s.low+1;
+	for(int i=0; i<size; i++){
+		cout<<s.p[i]<<endl;
+	}
+	return os;
 };
+
 int main(){
-SA a(10), b(3,5);
-b[3]=3; b[4]=4; b[5]=5;
-int i;
-for( i=0;i<10;i++)
-	a[i]=10-i;
-cout<< "printing a the first time" <<endl;
-cout<<a<<endl;
-cout<<"printing using []"<<endl;
-for( i=0;i<10;i++)
-	cout<<a[i]<<endl;
+	SA a(10), b(3,5);
+	b[3]=3;
+	b[4]=4;
+	b[5]=5;
+	int i;
+	for( i=0;i<10;i++){
+		a[i]=10-i;
+	}
+	cout<< "printing a the first time" <<endl;
+	cout<<a<<endl;
+	cout<<"printing using []"<<endl;
+	for( i=0;i<10;i++){
+		cout<<a[i]<<endl;
+	}
 	// write your own sort
 	// sort(a,10);
 	cout<<"printing a the second time" <<endl;
